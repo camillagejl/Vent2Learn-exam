38ee54a03c3b3ade18870b7a0bc5774f4a9e64d5
Camilla Dilling-Hansen <camilla@dilling.co>
2020-11-27 22:14:18 +0100

    Removed 'module' from module folder names, and added the few initial routes to app-routing

--- angular/Vent2Learn/src/app/app-routing.module.ts
+++ angular/Vent2Learn/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginViewComponent} from "./login/login-view/login-view.component";
+import {VentSelectionViewComponent} from "./vent-selection/vent-selection-view/vent-selection-view.component";
+import {ZoneOverviewModule} from "./zone-overview/zone-overview.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginViewComponent},
+  {path: 'vent-selection', component: VentSelectionViewComponent},
+  {path: 'zone-overview', component: ZoneOverviewModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/Vent2Learn/src/app/app.module.ts
+++ angular/Vent2Learn/src/app/app.module.ts
@@ -4,11 +4,11 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {LearningElementModule} from "./learning-element-module/learning-element.module";
-import {LoginModule} from "./login-module/login.module";
-import {SharedComponentsModule} from "./shared-components-module/shared-components.module";
-import {VentSelectionModule} from "./vent-selection-module/vent-selection.module";
-import {ZoneOverviewModule} from "./zone-overview-module/zone-overview.module";
+import {LearningElementModule} from "./learning-element/learning-element.module";
+import {LoginModule} from "./login/login.module";
+import {SharedComponentsModule} from "./shared-components/shared-components.module";
+import {VentSelectionModule} from "./vent-selection/vent-selection.module";
+import {ZoneOverviewModule} from "./zone-overview/zone-overview.module";
 
 @NgModule({
   declarations: [
