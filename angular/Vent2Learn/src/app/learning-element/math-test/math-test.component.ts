09d85973961fe55892dc2408527a3f08574683ea
Camilla Dilling-Hansen <camilla@dilling.co>
2020-11-28 22:04:43 +0100

    Added math for humidity in test component in LearningElement

--- angular/Vent2Learn/src/app/learning-element/math-test/math-test.component.ts
+++ angular/Vent2Learn/src/app/learning-element/math-test/math-test.component.ts
@@ -9,19 +9,19 @@ export class MathTestComponent implements OnInit {
 
   users = [
     {
-      ventilation: 1,
+      ventilation: 10,
       heating: 3
     },
     {
-      ventilation: 2,
+      ventilation: 10,
       heating: 5
     },
     {
-      ventilation: 4,
+      ventilation: 10,
       heating: 1
     },
     {
-      ventilation: 2,
+      ventilation: 9,
       heating: 7
     }
   ];
@@ -30,15 +30,16 @@ export class MathTestComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.calculateRoomTemperature());
+    console.log(`Room temperature: ${this.calculateRoomTemperature()}°C`);
+    console.log(`Room humidity: ${this.calculateRoomHumidity()}%`);
   }
 
   calculateRoomTemperature() {
 
-    // Variables are set in this function as they are not needed anywhere else.
+    // Variables are defined in this function as they are not needed anywhere else.
 
     // Min and max temperatures are set from recommended classroom temperatures +/- 2deg.
-    // The base temperature is calculated as the average value of those.
+    // The baseTemperature is calculated as the average value of those.
     const minTemperature = 18;
     const maxTemperature = 27;
     const baseTemperature = ( minTemperature + maxTemperature ) / 2;
@@ -51,8 +52,9 @@ export class MathTestComponent implements OnInit {
     let roomTemperature: number;
 
 
-    // For each user in the array, the userVentilation and userHeating is calculated as the average of each user's
-    // setting. The average for both will be between 0 and 10, which corresponds to that value of degrees.
+    // For each user in the array, the userVentilation and userHeating is added up and then calculated as the average
+    // of each user's setting. The average for both will be between 0 and 10, which corresponds to that value of
+    // degrees.
     this.users.forEach(user => {
       userVentilation = userVentilation + user.ventilation;
       userHeating = userHeating + user.heating;
@@ -73,8 +75,40 @@ export class MathTestComponent implements OnInit {
       roomTemperature = maxTemperature;
     }
 
+
     // Returns the final result for the roomTemperature.
     return roomTemperature;
+  }
+
+  calculateRoomHumidity() {
+    // Variables are defined in this function as they are not needed anywhere else.
+
+    // Min and max humidity are set from recommended classroom humidity in %.
+    // As the base humidity for this project, we use the maxHumidity, because the  ventilation can only bring the
+    // humidity down, not up.
+    const minHumidity = 30;
+    const maxHumidity = 60;
+
+    // With no user interference, the ventilation will not affect the roomHumidity.
+    let userVentilation = 0;
+
+    // roomHumidity is only calculated later.
+    let roomHumidity: number;
+
+
+    // For each user in the array, the userVentilation is added up.
+    this.users.forEach(user => {
+      userVentilation = userVentilation + user.ventilation;
+    });
+
+    // This line starts out by finding the average of all users' ventilation setting.
+    // Then, this is calculated to the corresponding percentage that should be subtracted from the maxHumidity.
+    userVentilation = ( userVentilation / this.users.length ) * ( maxHumidity - minHumidity ) / 10;
+
+    // The roomHumidity is the maxHumidity, minus the userVentilation as calculated above.
+    roomHumidity = maxHumidity - userVentilation;
+
+    return roomHumidity;
   }
 
 }
