56a3b57d750a691ba38ac6d10e4cda97c62d9189
Camilla Dilling-Hansen <camilla@dilling.co>
2020-12-04 19:35:43 +0100

    Added proper icons and styling to the bottom navigation

--- angular/Vent2Learn/src/app/navigation/bottom-toolbar/bottom-toolbar.component.ts
+++ angular/Vent2Learn/src/app/navigation/bottom-toolbar/bottom-toolbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Location} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-bottom-toolbar',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BottomToolbarComponent implements OnInit {
 
-  constructor() { }
+  currentRouteGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private location: Location,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+
+      let currentRoute;
+
+      currentRoute = this.location.path();
+
+      // The currentRouteGroup can have one of four strings as value - corresponding to our four menu buttons in the
+      // bottom navigation. The button that corresponds to the currentRouteGroup will get a 'selected' class and be
+      // styled so the user can see that that is the active path.
+
+      if (
+        currentRoute.includes('/vent-selection/' ||
+          currentRoute.includes('/time-selection/') ||
+          currentRoute.includes('/zone-overview/')
+        )) {
+        this.currentRouteGroup = 'vent';
+      }
+      if (currentRoute.includes('/settings/')) {
+        this.currentRouteGroup = 'settings';
+      }
+      if (currentRoute.includes('/tutorial/')) {
+        this.currentRouteGroup = 'help';
+      }
+      if (currentRoute.includes('/about/')) {
+        this.currentRouteGroup = 'about';
+      }
+
+    });
   }
 
 }
