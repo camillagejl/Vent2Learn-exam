6f1ec22feb599c176c856b47496fbd006ab80671
Camilla Dilling-Hansen <camilla@dilling.co>
2020-12-08 21:45:55 +0100

    Added a help button to the task

--- angular/Vent2Learn/src/app/learning-element/learning-element.module.ts
+++ angular/Vent2Learn/src/app/learning-element/learning-element.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MathTestComponent } from './math-test/math-test.component';
-import { TutorialStartingViewComponent } from './tutorial-starting-view/tutorial-starting-view.component';
-import { TutorialTask2ViewComponent } from './tutorial-task2-view/tutorial-task2-view.component';
-import { TutorialTask1ViewComponent } from './tutorial-task1-view/tutorial-task1-view.component';
-import { TutorialEndingViewComponent } from './tutorial-ending-view/tutorial-ending-view.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MathTestComponent} from './math-test/math-test.component';
+import {TutorialStartingViewComponent} from './tutorial-starting-view/tutorial-starting-view.component';
+import {TutorialTask2ViewComponent} from './tutorial-task2-view/tutorial-task2-view.component';
+import {TutorialTask1ViewComponent} from './tutorial-task1-view/tutorial-task1-view.component';
+import {TutorialEndingViewComponent} from './tutorial-ending-view/tutorial-ending-view.component';
 import {MatButtonModule} from "@angular/material/button";
 import {RouterModule} from "@angular/router";
-import { TutorialTask1ModalContentComponent } from './tutorial-task1-view/tutorial-task1-modal-content/tutorial-task1-modal-content.component';
+import {TutorialTask1ModalContentComponent} from './tutorial-task1-view/tutorial-task1-modal-content/tutorial-task1-modal-content.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatSliderModule} from "@angular/material/slider";
 import {FormsModule} from "@angular/forms";
 import {MatCardModule} from "@angular/material/card";
-
 
 @NgModule({
   declarations: [MathTestComponent, TutorialStartingViewComponent, TutorialTask2ViewComponent, TutorialTask1ViewComponent, TutorialEndingViewComponent, TutorialTask1ModalContentComponent],
@@ -31,4 +30,5 @@ import {MatCardModule} from "@angular/material/card";
     MatCardModule
   ]
 })
-export class LearningElementModule { }
+export class LearningElementModule {
+}
